c7043fc282f2d1ccbfab0b02a19b8e2b03f7f8a3
ttan158 <ttan158@aucklanduni.ac.nz>
2025-10-15 05:46:31 +1300

    Dashboard sidebar setup

---
+++ frontend/src/components/sidebar/app-sidebar.tsx
@@ -0,0 +1,31 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "../ui/sidebar";
+import SidebarMenuItems from "./sidebar-menu-items";
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-primary mt-4 mb-12 flex flex-col items-start justify-start px-2 text-3xl font-black tracking-widest uppercase">
+            <p className="text-lg">Octavia</p>
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItems />
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
