67244927d2cde9744ecff611fee18b5ab40dd82e
ttan158 <ttan158@aucklanduni.ac.nz>
2025-10-21 21:15:48 +1300

    Fixed deployment env

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -8,7 +8,7 @@ export const env = createEnv({
    */
   server: {
     BETTER_AUTH_SECRET: z.string(),
-    BETTER_AUTH_URL: z.string(),
+    BETTER_AUTH_URL: z.string().url(),
     DATABASE_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
